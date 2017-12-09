aeb5f09946da15f1b439f80aaccdd0d2eb9d2ba7
lechpoznan <zskgit@gmail.com>
2017-12-09 10:24:52 +0100

    asd

--- B/2017_grudzien_09/2_sortowanie.php
+++ B/2017_grudzien_09/2_sortowanie.php
@@ -8,12 +8,21 @@
     <?php
         $dir = "./test";
         if(!$folder = opendir($dir)){
-            closedir($fd);
+            closedir($folder);
             exit("Nie można otworzyć folderu");
         }else{
-
-            closedir($fd);
+            $sortuj = 0;
+            $dane = scandir($dir, $sortuj);
+            echo "<ul>";
+                foreach ($dane as $wartosc){
+                    if($wartosc != "." && $wartosc != "..")
+                        echo "<li>$wartosc</li>";
+                }
+            echo "</ul>";
+            closedir($folder);
         }
     ?>
+    <a href="2_sortowanie.php?sortuj=0">Sortuj rosnąco</a>
+    <a href="2_sortowanie.php?sortuj=1">Sortuj malejąco</a>
 </body>
 </html>
